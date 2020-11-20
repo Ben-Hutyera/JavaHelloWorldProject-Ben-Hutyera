a5281bbb94e57cda3bb59272fe425575a2f66a1d
Ben-Hutyera <70227921+Ben-Hutyera@users.noreply.github.com>
2020-11-19 22:27:53 -0500

    Add files via upload

---
+++ paintWall.java
@@ -0,0 +1,37 @@
+package src;
+import java.util.Scanner;
+public class paintWall {
+	   public static void main(String[] args) {
+		   
+		  
+		  
+	      Scanner scnr = new Scanner(System.in);
+	      
+	      int userInt;
+	      double userDouble;
+	      char userChar;
+	      String userString;
+	      int userDoubleInt;
+	      
+	      System.out.println("Enter integer:");
+	      userInt = scnr.nextInt();
+	      
+	      System.out.println("Enter double:");
+	      userDouble = scnr.nextDouble();
+	      
+	      System.out.println("Enter character:");
+	      userChar = scnr.next().charAt(0);
+	      
+	      System.out.println("Enter string:");
+	      userString = scnr.next();
+	      
+	      System.out.println(userInt+" "+userDouble+" "+userChar+" "+userString);
+	      System.out.println(userString+" "+userChar+" "+userDouble+" "+userInt);
+	      
+	      userDoubleInt = (int)userDouble;
+	      System.out.println(userDouble+" cast to an integer is "+userDoubleInt);
+	      
+	      scnr.close();
+	   }
+	}
+
